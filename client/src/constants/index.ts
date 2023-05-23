515befeab3630056378dc2262924c6e221935a53
Bhavika <bhavikashah8@gmail.com>
2023-05-23 09:50:15 +0530

    first commit

---
+++ client/src/constants/index.ts
@@ -0,0 +1,27 @@
+export const propertyReferralsInfo = [
+    {
+        title: "Milk",
+        percentage: 64,
+        color: "#6C5DD3",
+    },
+    {
+        title: "Ghee",
+        percentage: 40,
+        color: "#7FBA7A",
+    },
+    {
+        title: "Cheese",
+        percentage: 50,
+        color: "#FFCE73",
+    },
+    {
+        title: "Butter",
+        percentage: 80,
+        color: "#FFA2C0",
+    },
+    {
+        title: "Others",
+        percentage: 15,
+        color: "#F45252",
+    },
+];
